4d359972a586794609827c4c52bfacdec10ed7a8
Hirendra <itshirendra@gmail.com>
2021-05-25 21:30:13 +0530

    updated Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,9 @@ function Footer() {
                 <h2>Newsefy</h2>
                 <p>Get trending and latest news all the time and ensure a informative life.</p>
             </div>
+            <div className="footerSupport">
+                <h3>We are coming soon to save your time.</h3>
+            </div>
         </div>
     )
 }
